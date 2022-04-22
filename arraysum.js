ef430d41d69ebff0076bb7c79b96d7194c0a0d4c
Nerissa Dorlus <nerissadorlus@Tdas-Pro-3.local>
2022-04-22 15:55:29 -0400

    updated another way to solve sum of arrays

--- arraysum.js
+++ arraysum.js
@@ -3,7 +3,17 @@ const myArr = [5, 7, 10, 2, 1];
 function sumOfArray(arr) {
   let result = 0;
   for (let i = 0; i < arr.length; i++) {
-    result += arr[i];
+    result += arr[i]; // result = result + arr[i]
+  }
+  return result;
+}
+console.log(sumOfArray(myArr));
+
+//--------------------------------------------------------------------------
+function sumOfArray(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result = result + arr[i];
   }
   return result;
 }
